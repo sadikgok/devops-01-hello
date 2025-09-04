2af5e96b986f359cd78703efa7bff62ca71f2050
sadikgok <61986112+sadikgok@users.noreply.github.com>
2025-09-04 06:35:08 +0300

    DevOps-01-Hello
    My first project

---
+++ src/main/java/com/example/devops01hello/AppMain.java
@@ -0,0 +1,13 @@
+package com.example.devops01hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devops01hello/Devops01HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops01hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops01HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
